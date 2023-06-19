ff5a8755f4bea959edcde036028e2dbc743cc740
Huynh Manh Tam <huynhtam258@gmail.com>
2023-06-19 23:10:23 +0700

    key repository

---
+++ src/core/repositories/key.reposiory.ts
@@ -0,0 +1,18 @@
+import { inject } from "inversify";
+import { Key } from "../interfaces/contracts";
+import { IKeyRepository } from "../interfaces/reponsitories/key-repository.interface";
+import { BaseRepository } from "./base.repositories";
+import { Locator } from "../../constants/app.constant";
+import { Model } from "mongoose";
+
+export class KeyRepository extends BaseRepository<Key> implements IKeyRepository{
+    constructor(
+        @inject(Locator.KEY_MODEL) readonly keyModel: Model<Key>
+    ) {
+        super(keyModel);
+    }
+
+    async findByUserId(userId: string): Promise<Key | null> {
+        return await this.findFirst({ user: userId})
+    }
+}
