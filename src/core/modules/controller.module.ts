e68e1638b92e121d912cc6aea768d526f72bea42
HuynhManhTam <huynhtam258@gmail.com>
2023-06-20 10:19:56 +0700

    import auth controller into controller container module

--- src/core/modules/controller.module.ts
+++ src/core/modules/controller.module.ts
@@ -1,7 +1,9 @@
 import { ContainerModule } from "inversify";
 import { BaseController } from "../controllers/base.controller";
 import { Locator } from "../../constants/app.constant";
+import { AuthController } from "../controllers/auth.controller";
 
 export const controllerContainerModule: ContainerModule = new ContainerModule((bind) => {
     bind<BaseController>(Locator.BASE_CONTROLLER).to(BaseController).inSingletonScope()
+    bind<AuthController>(Locator.AUTH_CONTROLLER).to(AuthController).inRequestScope()
 })
